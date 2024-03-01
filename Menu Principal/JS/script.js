14f97ca001703b844838140c7b97027b76ff6c9d
DigoPires <rodrigo_pires2806@outlook.com>
2024-03-01 17:14:07 -0300

    Commit

--- Menu Principal/JS/script.js
+++ Menu Principal/JS/script.js
@@ -5,6 +5,10 @@ const navBar = document.querySelector('.nav-bar');
 const closeMenu = document.querySelector('.mobile-menu-fechar-x');
 const activeClass = 'active';
 
+const logo = document.querySelector(".logo-container");
+const menu = document.querySelector(".menu-principal");
+const footer = document.querySelector(".footer");
+
 function setTopo(){
     let static = window.pageYoffset
     scrollTo(0, static);
@@ -22,10 +26,18 @@ document.addEventListener('scroll', () => { //bloqueia o scroll quando aberto a 
 
 window.addEventListener("scroll", () => {
     let header = document.querySelector("#header");
-    header.classList.toggle("rolagem", window.scrollY > 20);
+    header.classList.toggle("rolagem", window.scrollY > 1);
 });
 
 closeMenu.addEventListener("click", () => navBar.classList.remove('active'));
+
+// if(navBar.addEventListener("click")){
+//     navBar.classList.remove('active')
+// }
+
+logo.addEventListener("click", () => navBar.classList.remove('active'));
+menu.addEventListener("click", () => navBar.classList.remove('active'));
+footer.addEventListener("click", () => navBar.classList.remove('active'));
 
 //script carrosel
 let currentIndex = 0;
