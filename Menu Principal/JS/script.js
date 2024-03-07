22054e82f39fd3e64d3bc3baceb4cb94eca62c69
jeffersonsjsilva <jeffe.santana19@gmail.com>
2024-03-07 16:19:50 -0300

    repondo

---
+++ Menu Principal/JS/script.js
@@ -0,0 +1,82 @@
+// script hamburguer
+const mobileMenu = document.querySelector('.mobile-menu');
+const navBar = document.querySelector('.nav-bar');
+const closeMenu = document.querySelector('.mobile-menu-fechar-x');
+const activeClass = 'active';
+
+const logo = document.querySelector(".logo-container");
+const menu = document.querySelector("main");
+const footer = document.querySelector("footer");
+
+function setTopo(){
+    let static = window.pageYoffset
+    scrollTo(0, static);
+}
+
+mobileMenu.addEventListener("click", () => {
+    navBar.classList.add(activeClass);
+});
+
+document.addEventListener('scroll', () => { //bloqueia o scroll quando aberto a navbar no mobile
+    if(navBar.classList.contains(activeClass)) {
+        setTopo();
+    }
+});
+
+window.addEventListener("scroll", () => {
+    let header = document.querySelector("#header");
+    header.classList.toggle("rolagem", window.scrollY > 1);
+});
+
+closeMenu.addEventListener("click", () => navBar.classList.remove('active'));
+
+// if(navBar.addEventListener("click")){
+//     navBar.classList.remove('active')
+// }
+
+
+
+// fechar ao clicar fora da nav-bar
+logo.addEventListener("click", () => navBar.classList.remove('active'));
+menu.addEventListener("click", () => navBar.classList.remove('active'));
+footer.addEventListener("click", () => navBar.classList.remove('active'));
+
+
+//tentando criar função pra abrir e fechar area-restrita
+let btn = document.querySelector('.area-restrita')
+let form = document.querySelector('.restrita');
+
+btn.addEventListener('click', function() {
+    if(form.style.display != 'flex'){
+    form.style.display = 'flex';
+    form.style.flexDirection ='column';
+    return;
+    }
+    form.style.display = 'none'
+});
+
+
+//script carrosel
+let currentIndex = 0;
+
+function showSlide(index) {
+    const wrapper = document.querySelector('.carousel-wrapper');
+    const itemWidth = document.querySelector('.carousel-item').clientWidth;
+
+    currentIndex = index;
+    wrapper.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+}
+
+function nextSlide() {
+    const totalItems = document.querySelectorAll('.carousel-item').length;
+    currentIndex = (currentIndex + 1) % totalItems;
+    showSlide(currentIndex);
+}
+
+function prevSlide() {
+    const totalItems = document.querySelectorAll('.carousel-item').length;
+    currentIndex = (currentIndex - 1 + totalItems) % totalItems;
+
+    showSlide(currentIndex);
+}
+
