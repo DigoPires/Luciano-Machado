74de745c95243abaf60e6fe2d67ec3fcdeee2b7b
jeffersonsjsilva <jeffe.santana19@gmail.com>
2024-03-21 19:26:29 -0300

    arrumano links dos icones

--- views/agenda.php
+++ views/agenda.php
@@ -101,11 +101,21 @@ include_once ('../models/conexao.php');
     <footer id="rodape">
         <p>Entre em contato para realizar seu orçamento</p>
         <div class="icones">
-            <a href="https://www.instagram.com/lumachadocasamentos/" target="_blank"><img class="icones-img"
-                    src="./views/IMG/icones/instagram.png" alt="Contato Instagram" class="contatos"></a>
-            <a href="https://web.whatsapp.com/" target="_blank"> <img class="icones-img" src="./views/IMG/icones/whatsapp.png" alt="Contato WhatsApp"
-                    class="contatos"></a>
-            <a href="https://www.google.com/intl/pt-BR/gmail/about/" target="_blank"> <img class="icones-img" src="./views/IMG/icones/gmail.png" alt="Contato Gmail" class="contatos"></a>
+
+            <a href="https://www.instagram.com/lumachadocasamentos/" target="_blank">
+                <img class="icones-img"
+                    src="./IMG/icones/instagram.png" alt="Contato Instagram" class="contatos">
+            </a>
+
+            <a href="https://web.whatsapp.com/" target="_blank">
+                <img class="icones-img" src="./IMG/icones/whatsapp.png" alt="Contato WhatsApp"
+                class="contatos">
+            </a>
+
+            <a href="https://www.google.com/intl/pt-BR/gmail/about/" target="_blank">
+                <img class="icones-img" src="./IMG/icones/gmail.png" alt="Contato Gmail" class="contatos">
+            </a>
+
         </div>
         <div class="copyright-div">Design by Alan, Jefferson, Julia, Rodrigo e Ryan - 2024&copy;</div>
     </footer>
